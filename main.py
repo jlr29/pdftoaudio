9bb8cbab89389ca32267e3f0a8938e1b11a6f55d
jlr29 <giova9975@gmail.com>
2023-02-15 15:06:51 +0100

    project done

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-def printo():
-    pycharm = "Pycharm"
-    print(pycharm)
+import pyttsx3,PyPDF2,os,sys
 
+pdfreader = PyPDF2.PdfReader(open('storia.pdf' , 'rb'))
+speaker =pyttsx3.init()
 
-if __name__ == '__main__':
-    print("Pycharm ecc")
-    printo()
+for page_num in range(len(pdfreader.pages)):
+    text = pdfreader.pages[page_num].extract_text()
+    clean_text = text.strip().replace('\n',' ')
+    #print(clean_text)
+
+speaker.save_to_file(clean_text,'test.mp3')
+#os.rename("test.mp3", "joler/Downloads/audible.mp3")
+speaker.runAndWait()
+
+speaker.stop()
